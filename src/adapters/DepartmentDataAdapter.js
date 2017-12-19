ae580466eb5dfd277b9a72c4212f177ce8bfda1a
github username <github email address>
2017-12-19 10:03:31 -0500

    changed links

--- src/adapters/DepartmentDataAdapter.js
+++ src/adapters/DepartmentDataAdapter.js
@@ -1,5 +1,5 @@
-// const path = 'https://mb-backend.herokuapp.com/api/v1/departmentdatum'
-const path = 'http://localhost:3000/api/v1/departmentdatum'
+const path = 'https://mb-backend.herokuapp.com/api/v1/departmentdatum'
+// const path = 'http://localhost:3000/api/v1/departmentdatum'
 
 export default class DepartmentDataAdapter {
 
